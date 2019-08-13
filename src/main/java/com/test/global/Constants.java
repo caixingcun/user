55516dc841a6fe4d9a9741e913977dcd02e76661
caixc <caixc@anchnet.com>
2019-08-13 14:35:40 +0800

    新增一个创业板指数

--- src/main/java/com/test/global/Constants.java
+++ src/main/java/com/test/global/Constants.java
@@ -2,6 +2,6 @@ package com.test.global;
 
 public class Constants {
     public static final String[] INDEXS = {
-        "中证500","红利指数","中证红利","恒生指数","上证50","国企指数","上证180","沪深300"
+        "中证500","红利指数","中证红利","恒生指数","上证50","国企指数","上证180","沪深300","创业板指"
     };
 }
